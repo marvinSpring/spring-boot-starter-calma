30bde06146f329b8f9d590751adac1bbec49159d
marvin <1261626796@qq.com>
2020-12-14 14:21:50 +0800

    Sms和钉钉都可以发送，可以选择SMS或者钉钉但是不能全选

--- src/main/java/com/marvin/util/SmsNoticeSendComponent.java
+++ src/main/java/com/marvin/util/SmsNoticeSendComponent.java
@@ -1,32 +1,40 @@
 package com.marvin.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.marvin.model.PiracyNotice;
 import com.marvin.model.SmsNotice;
 import com.marvin.util.sms.SendSmsClient;
 
-public class SmsNoticeSendComponent<T extends PiracyNotice> implements NoticeSendComponent<PiracyNotice > {//短信发送的组件
+public class SmsNoticeSendComponent<T extends PiracyNotice> implements NoticeSendComponent<PiracyNotice> {// 短信发送的组件
 
 	private final PiracyNoticeTextResolver<PiracyNotice> resolver;
-	
+
 	private final SendSmsClient client;
-	
-	public SmsNoticeSendComponent(PiracyNoticeTextResolver<PiracyNotice> resolver,Client client) {
-		this.client = (SendSmsClient)client;
+
+	public SmsNoticeSendComponent(PiracyNoticeTextResolver<PiracyNotice> resolver, Client client) {
+		this.client = (SendSmsClient) client;
 		this.resolver = resolver;
 	}
-	
+
 	@Override
-	public void send(PiracyNotice piracyNotice) {//这里应该使用smsModel，然后其中封装手机号accesskey等信息
-		String text = resolver.resolve(piracyNotice);//将异常解析成便于阅读的结构
-		SmsNotice notice = new SmsNotice();
-		notice.setPhoneNumbers("");
-		notice.setAccessKey("");
-		notice.setSecret("");
-		notice.setParam(String.valueOf(text.length()));//TODO:通用短信签名和模板没下来，只能发数字，暂时就是异常的长度
-		notice.setTemplateCode("");
-		notice.setRegionId("");
-		notice.setSignName("");
-		client.doSend(notice);
+	public void send(PiracyNotice piracyNotice) {// 这里应该使用smsModel，然后其中封装手机号accesskey等信息
+		Map<String, Object> map = this.smsResolve(piracyNotice);// 将异常解析成短信模板结构
+		SmsNotice smsNotice = new SmsNotice();
+		smsNotice.setParam(map);
+		client.doSend(smsNotice);
+	}
+
+	private Map<String,Object> smsResolve(PiracyNotice piracyNotice) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("project",piracyNotice.getProjectName());
+		map.put("time",piracyNotice.getCreateTime());
+		for (String x : piracyNotice.getExceptionMessage()) {
+			map.put("causeBy",x.substring(x.indexOf(":")+1));
+		}
+		return map;
 	}
 
 }
